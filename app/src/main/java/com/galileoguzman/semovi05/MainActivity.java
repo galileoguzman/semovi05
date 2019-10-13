2146edb532b482a6d12a6f6560a700b6ddb33077
galileoguzman <galileoguzman@gmail.com>
2019-10-13 09:06:42 -0500

    Added webview

--- app/src/main/java/com/galileoguzman/semovi05/MainActivity.java
+++ app/src/main/java/com/galileoguzman/semovi05/MainActivity.java
@@ -3,12 +3,20 @@ package com.galileoguzman.semovi05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://capacitacionescoco.com");
     }
 }
